efb49c1333e8a032420c3e3f33352b63e2831dc4
Pionxzh <pionxzh@users.noreply.github.com>
2017-10-20 20:38:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ let asset = {
     materialIcon: "https://fonts.googleapis.com/icon?family=Material+Icons",
     materialCss: "https://code.getmdl.io/1.3.0/material.indigo-pink.min.css",
     materialJs: "https://code.getmdl.io/1.3.0/material.min.js"
-}
+};
 
 let $head = document.getElementsByTagName('head')[0];
 
@@ -36,7 +36,7 @@ function injectCss(name, src) {
 
 function injectStyle() {
     let rule = `
-    .demo-layout-transparent { 
+    .demo-layout-transparent {
         background: url("https://i.imgur.com/zhgtd7S.jpg") center / cover;
     }
     .demo-layout-transparent .mdl-layout__header, .mdl-layout__drawer-button {
@@ -59,16 +59,17 @@ function getTable(dom) {
 }
 
 $(function() {
-    injectCss('material-icon', asset.materialIcon);
     injectCss('material-css', asset.materialCss);
+    injectCss('material-icon', asset.materialIcon);
     injectStyle();
     injectJs('material-js', asset.materialJs);
     $("frame[src*='take_course.php']").on('load', function() {
         console.log('Take_course loaded');
         let courseTable = getTable(this.contentDocument);
         console.log(courseTable);
-        let $body = $('html').prepend(document.createElement('body'));
-        $body.prepend(`<div class="demo-layout-transparent mdl-layout mdl-js-layout">
+        $('head').after();
+        let $body = document.createElement('body');
+        $($body).html(`<div class="demo-layout-transparent mdl-layout mdl-js-layout">
                             <header class="mdl-layout__header mdl-layout__header--transparent">
                                 <div class="mdl-layout__header-row">
                                 <span class="mdl-layout-title">eCourse++</span>
@@ -91,5 +92,6 @@ $(function() {
                             </div>
                             <main class="mdl-layout__content"></main>
                         </div>`);
+        $('head').after($body);
     });
 });
